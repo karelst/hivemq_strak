d4d5a0d9c84c02fec72818d26c796b9ea84fbcb6
karelst <karel.steidl@gmail.com>
2026-01-05 16:58:06 +0100

    Initial commit

--- server_MqClass.py
+++ server_MqClass.py
@@ -1,69 +1,113 @@
 from flask import Flask, render_template, request
 # from weather import get_current_weather
 from waitress import serve
-import strmqtt_bg
+from strmqtt_Class import MqttApp
 import time
+#import strmqtt_bg
+
 
 app = Flask(__name__)
-
+Mq = MqttApp()
 
 @app.route('/')
 @app.route('/index')
+@app.route('/index.html')
 def index():
     print(f'---RUN server/index() RUN----')    
     # return ('BlackBox0 TEXT')
     return render_template('index.html')
 
 
+@app.route('/myaction')
+def maction():
+    psw = request.args.get('psw').lower()
+    print(f'--reload myaction.html--psw: {psw} SUBMIT---***')
+    if psw != "strak" :
+        return render_template('index.html')
+    return action('psw', psw )
+    
+def action(subject ,data):
+    #print(f'- - action ( subject={subject} :: data={data} ) ***')
+    if not Mq.connection_ok:
+        Mq.connect_broker()  
+    # if  Mq.connection_ok:
+    #     Mq.publish('InfoUpdate','1')
+    
+    topic_values = Mq.get_topic_values()
+    #print(f'--- new data: {topic_values}')
+    #print(f'action->RENDER myaction.html : subject={subject} :: data={data}***')
+    return render_template('myaction.html',
+                           title=data,
+                           isalive=topic_values["isalive"],                          
+                           WillTopic=topic_values["WillTopic"],
+                           teploty=topic_values["teploty"],
+                           pir=topic_values["pir"],
+                           TeplarnaStav = topic_values["TeplarnaStav"],
+                           InfoUpdate = topic_values["InfoUpdate"],
+                           TeplarnaOnOff = topic_values["TeplarnaOnOff"],
+                           STRAKbeep = topic_values["STRAKbeep"]
+                           )
+
+    
 @app.route('/action_password')
 def action_password():
     print(f'---RUN action_password() RUN----')    
     # return ('BlackBox0 TEXT')
     return render_template('action_password.html')
  
- 
+
+@app.route('/button_stop')
+def stop_action():
+    #psw = request.args.get('psw')
+    print(f'***STOP***STOP***STOP***STOP***STOP***')
+    #return action('stop', 1)
+    Mq.disconnect_broker()
+    print(f'- -  *** DISCONNECT mqtt broker!')
+    return render_template('index.html')   
+
+
 @app.route('/action_confirm')
 def action_confirm():
     #action_confirm?cmd=other&psw=alfa
     cmd = request.args.get('cmd')
     psw = request.args.get('psw')
-    print(f'***Confirm---{cmd}*{psw}---***')
+    psw = psw.lower()
+    print(f'*** RUNNING: action_confirm()---{cmd} / {psw}---***')
 
     if psw != "strak":
-      return action('psw', psw )
-    #return render_template('index.html')
+      #return action('psw', psw )
+      return render_template('index.html')
     
     if cmd == 'update':
-        strmqtt_bg.publish('InfoUpdate',1)
-    elif cmd == 'beep':
-        strmqtt_bg.publish('STRAKbeep',1)
-    elif cmd == 'off':
-    #    strmqtt_bg.publish('TeplarnaONOff',0)
-        print(f'***OTHER--{cmd}***')
-    elif cmd == 'on':
-    #    strmqtt_bg.publish('TeplarnaONOff',1)
-        print(f'***OTHER--{cmd}***')
+        Mq.publish('InfoUpdate','1')
+    elif cmd == 'beep_On':
+        Mq.publish('STRAKbeep','1')
+    elif cmd == 'TeplarnaOnOff_Off':
+        Mq.publish('TeplarnaOnOff','Off') 
+    ##	mqttClient20.Publish(&S"TeplarnaOnOff",&S"Off",1)
+    #   Mq.publish('TeplarnaONOff','Off') 
+        print(f'*** publish( --{cmd}***')
+    elif cmd == 'TeplarnaOnOff_On':
+        Mq.publish('TeplarnaOnOff','On')
+    ##	mqttClient20.Publish(&S"TeplarnaOnOff",&S"On",1)
+     #   Mq.publish('TeplarnaOnOff','On') 
+        print(f'*** publish( --{cmd}***')
     elif cmd == 'other':
-    #    strmqtt_bg.publish('STRAKbeep',1)
-        print(f'***OTHER--{cmd}***')
-        
+    #    Mq.publish('STRAKbeep',1)
+        #print(f'***OTHER--{cmd}***')
+        print(f'*** cmd OTHER(: --{cmd}***')
+    else:
+        print(f'*** ELSE : --{cmd}***')
     return render_template('action_confirm.html',  cmd=cmd, psw=psw)
 
 
-
-@app.route('/myaction')
-def maction():
-    psw = request.args.get('psw')
-    print(f'{psw} SUBMIT---***')
-    if psw == "strak":
-      return action('psw', psw )
-    return render_template('index.html')
+@app.route('/myaction_confirm')
+def maction_confirm():
+    return action('psw', 'strak' )
     
-@app.route('/button_stop')
-def stop_action():
-    #psw = request.args.get('psw')
-    print(f'***STOP***STOP***STOP***STOP***STOP***')
-    return action('stop', 1 )
+    # return render_template('action_password.html')
+    # return render_template('index.html')
+
 
 
 # @app.route('/button_HeatOn')
@@ -79,37 +123,6 @@ def stop_action():
 #     return action('TeplarnaOnOff', 'Off' )
 
 
-def action(subject ,data):
-    print(f'{subject} :: {data}***')
-    #     
-    strmqtt_bg.connect_broker()
-    
-    #time.sleep(6)
-    # strmqtt_bg.disconnect_broker()
-    # 
-    if subject == 'stop':
-        strmqtt_bg.disconnect_broker()
-        return render_template('index.html')   
-    
-    topic_values = strmqtt_bg.get_topic_values()
-    #print(f'--- new data: {topic_values}')
-    
-    
-    print(f'---RENDER server/myaction()----')
-    return render_template('myaction.html',
-                           title=data,
-                           isalive=topic_values["isalive"],                          
-                           WillTopic=topic_values["WillTopic"],
-                           teploty=topic_values["teploty"],
-                           pir=topic_values["pir"],
-                           TeplarnaStav = topic_values["TeplarnaStav"],
-                           InfoUpdate = topic_values["InfoUpdate"],
-                           TeplarnaOnOff = topic_values["TeplarnaOnOff"],
-                           STRAKbeep = topic_values["STRAKbeep"]
-                           
-                           )
-
-
 
 @app.route('/xxxxxxx', methods=["GET", "POST"])
 def home():
@@ -118,9 +131,9 @@ def home():
         if "my_bep" in request.form:
                 # name = request.args.get('name')
                 print(f'***BEEP***BEEP***BEEP***BEEP***BEEP***')
-                strmqtt_bg.publish('STRAKbeep',1)
+                Mq.publish('STRAKbeep',1)
     
-    topic_values = strmqtt_bg.get_topic_values()
+    topic_values = Mq.get_topic_values()
     #print(f'--- new data: {topic_values}')
     
     print(f'---RENDER server/myaction()----')
@@ -175,15 +188,15 @@ def beep_action():
     #psw = request.args.get('psw')
     print(f'***BEEP***BEEP***BEEP***BEEP***BEEP***')
     #return action('beep', 1 )
-    strmqtt_bg.publish('STRAKbeep',1)
-    # strmqtt_bg.publish('STRAKbeep','1')
+    Mq.publish('STRAKbeep',1)
+    # Mq.publish('STRAKbeep','1')
  """
     
     
     # # return render_template("index.html", message=message)
   
     #psw = request.args.get('psw')
-    # strmqtt_bg.publish('STRAKbeep','1')
+    # Mq.publish('STRAKbeep','1')
 
 
 """ EXAMPLE
---
+++ strmqtt_Class.py
@@ -0,0 +1,124 @@
+#strmqtt_bg.py
+import paho.mqtt.client as mqtt
+import ssl
+import time
+
+
+class MqttApp:
+    def __init__(self):
+        self.client = None
+        self.connection_ok = False
+
+        # slovník posledních hodnot témat
+        self.topic_values = {
+            "STRAKbeep": "none",
+            "STRAKbeepSubs": "none",
+            "InfoUpdate": "none",
+            "TeplarnaOnOff": -1,
+            "TeplarnaOnOffSubs": "none",
+            "isalive": -1,
+            "TeplarnaStav": "none",
+            "teploty": "none",
+            "pir": "none",
+            "WillTopic": "none"
+        }
+
+        self.topics = list(self.topic_values.keys())
+
+    # ==================================================
+    # MQTT CALLBACKS
+    # ==================================================
+    def on_connect(self, client, userdata, flags, rc):
+        print(f"Connected with result code {rc}")
+
+        for topic in self.topics:
+            client.subscribe(topic)
+        
+        client.publish('InfoUpdate','1')
+
+    def on_message(self, client, userdata, msg):
+        payload = msg.payload.decode()
+
+        if msg.topic not in ("isalive", "WillTopic"):
+            print(f"Received topic '{msg.topic}' = '{payload}'")
+        else:
+            print(".", end="", flush=True)
+
+        self.topic_values[msg.topic] = payload
+        self.check_connection()
+
+    # ==================================================
+    # CONNECTION MANAGEMENT
+    # ==================================================
+    def connect_broker(self):
+    #    print(">>>>>>>>>> connect_broker() START >>>>>>>>>>")
+
+        if self.connection_ok:
+            #print(">>>>>>>>>> connection already OK >>>>>>>>>>")
+            return 
+
+        print("---------- NEW CONNECTION ----------")
+
+        self.client = mqtt.Client()
+
+        # TLS / SSL
+        self.client.tls_set(
+            tls_version=ssl.PROTOCOL_TLSv1_2
+        )
+
+        self.client.username_pw_set("dashboard2", "Legolas007")
+
+        # callbacks
+        self.client.on_connect = self.on_connect
+        self.client.on_message = self.on_message
+
+        broker = "f3d0981b4d84442f9406f8b208f421b0.s1.eu.hivemq.cloud"
+        port = 8883
+
+        self.client.connect(broker, port, 60)
+        self.client.loop_start()
+
+        time.sleep(1)
+        self.connection_ok = self.client.is_connected()
+
+    def check_connection(self):
+        if self.client:
+            self.connection_ok = self.client.is_connected()
+        return self.connection_ok
+
+    def disconnect_broker(self):
+        if not self.client:
+            return
+
+        print("Exiting...")
+        self.client.loop_stop()
+        self.client.disconnect()
+        self.connection_ok = False
+
+    # ==================================================
+    # PUBLISH / ACCESSORS
+    # ==================================================
+    def publish(self, topic, message):
+        if not self.check_connection():
+            print("⚠️  Not connected, publish skipped")
+            return
+
+        print(f"*** publish({topic}, {message}) ***")
+        result = self.client.publish(topic, message)
+        print(f"Publish result: {result}")
+
+    def get_topic_values(self):
+        return self.topic_values.copy()
+
+
+
+if __name__ == "__main__":
+    app = MqttApp()
+
+    app.connect_broker()
+
+    print("---------- SLEEP 25 ----------")
+    time.sleep(25)
+
+    print("---------- DISCONNECT ----------")
+    app.disconnect_broker()
